68115db6e6b9f46fbd1e8ebf9685f9bb9971a080
Oleh Ovcharenko <oleh.ovcharenko@arbitas.com>
2023-09-29 13:01:00 +0300

    add swagger to interface and dto

--- src/task/dtos/cities-population-response.dto.ts
+++ src/task/dtos/cities-population-response.dto.ts
@@ -1,5 +1,7 @@
 import { ICityPopulation } from '../interfaces/city-population.interface';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CitiesPopulationResponseDTO {
+  @ApiProperty({ type: [ICityPopulation] })
   citiesPopulation: ICityPopulation[];
 }
--- src/task/interfaces/city-population.interface.ts
+++ src/task/interfaces/city-population.interface.ts
@@ -1,4 +1,12 @@
-export interface ICityPopulation {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ICityPopulation {
+  @ApiProperty({ description: 'Name of the city', example: 'Dnipro' })
   city: string;
+
+  @ApiProperty({
+    description: 'Number of residents in the city',
+    example: 10000,
+  })
   count: number;
 }
