17d08f8776923f80b228a70c2fe17b3dcc8c4c1c
Oleh Ovcharenko <oleh.ovcharenko@arbitas.com>
2023-10-01 14:33:36 +0300

    add interceptor

--- src/interceptors/logger.interceptor.ts
+++ src/interceptors/logger.interceptor.ts
@@ -4,7 +4,7 @@ import {
   ExecutionContext,
   CallHandler,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { catchError, lastValueFrom, Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpService } from '@nestjs/axios';
 
@@ -18,7 +18,7 @@ export class LoggingInterceptor implements NestInterceptor {
     const response = context.switchToHttp().getResponse();
 
     return next.handle().pipe(
-      map((data) => {
+      map(async (data) => {
         const requestDuration = Date.now() - start;
         const requestData = request.body;
         const responseData = data;
@@ -31,15 +31,33 @@ export class LoggingInterceptor implements NestInterceptor {
           httpStatus,
         });
 
-        this.httpService.post('http://localhost:8765/logging', {
-          requestDuration,
-          requestData,
-          responseData,
-          httpStatus,
-        });
+        try {
+          this.sendToLoggingService({
+            requestDuration,
+            requestData,
+            responseData,
+            httpStatus,
+          });
+        } catch (error) {
+          console.error('Error sending data to logging endpoint:', error);
+        }
 
         return data;
       }),
+      catchError((error) => {
+        return throwError(error);
+      }),
     );
+  }
+
+  private async sendToLoggingService(data: any): Promise<void> {
+    try {
+      await lastValueFrom(
+        this.httpService.post('http://localhost:8765/logging', data),
+      );
+    } catch (error) {
+      console.error('Error sending data to logging endpoint:', error);
+      throw error;
+    }
   }
 }
